f2b937da40af922535184329b473c3b113505a77
Joseph Ruane <jeruane@gmail.com>
2015-12-05 03:04:33 -0500

    +compiled common, test for sym working
    sym test is working if input is the blocksize. sym encrypt/decrypt isnt
    being padded currently

--- test.py
+++ test.py
@@ -1,13 +1,14 @@
+from os import urandom
 import common
 
-# sym_input = "this is a test"
-# sym_aes_key = ... # keygen or load from file.
-# sym_ciph, iv  = Symmetric_Encrypt(sym_input, AES_Key)
-# sym_plaintext = Symmetric_Decrypt(sym_ciph, AES_Key, iv)
-# if sym_input == sym_plaintext:
-#     print("symmetric encrypt success")
-# else:
-#     print("symmetric encrypt failed")
+sym_input     = "1234567890123456789012345678901234567890123456789012345678901234"
+sym_aes_key   = urandom(32) # 256 bit AES key
+sym_ciph, iv  = common.Symmetric_Encrypt(sym_input, sym_aes_key)
+sym_plaintext = common.Symmetric_Decrypt(sym_ciph,  sym_aes_key, iv)
+if sym_input == sym_plaintext:
+    print("symmetric encrypt success")
+else:
+    print("symmetric encrypt failed")
 
-# Asymmetric_Encrypt(Pub_Key, Clear_Text)
-# Asymmetric_Decrypt(Pri_Key, Ciph_Text)
+# common.Asymmetric_Encrypt(Pub_Key, Clear_Text)
+# common.Asymmetric_Decrypt(Pri_Key, Ciph_Text)
