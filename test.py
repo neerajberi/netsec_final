75218dd5dcbc3c86a63f4437ac6d531372f19cfe
Joseph Ruane <jeruane@gmail.com>
2015-12-05 02:06:16 -0500

    prototyped a test file
    prototype tests for common functions

---
+++ test.py
@@ -0,0 +1,13 @@
+import common
+
+# sym_input = "this is a test"
+# sym_aes_key = ... # keygen or load from file.
+# sym_ciph, iv  = Symmetric_Encrypt(sym_input, AES_Key)
+# sym_plaintext = Symmetric_Decrypt(sym_ciph, AES_Key, iv)
+# if sym_input == sym_plaintext:
+#     print("symmetric encrypt success")
+# else:
+#     print("symmetric encrypt failed")
+
+# Asymmetric_Encrypt(Pub_Key, Clear_Text)
+# Asymmetric_Decrypt(Pri_Key, Ciph_Text)
