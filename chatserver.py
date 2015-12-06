8feb6352f806072afa8108b52ced5726b3d642d5
Joseph Ruane <jeruane@gmail.com>
2015-12-06 02:38:17 -0500

    this was supposed to be in the last commit.

--- chatserver.py
+++ chatserver.py
@@ -1,77 +1,35 @@
 # chat server
 # python 3.5
 
-import sys, socket, getopt, select
+import sys, socket, getopt, select, common
 
-# broadcasts chat messages to all clients
-# s = socket of client that sent the message, m = message
-def broadcast(s, m):
-    for sock in clients:
-        if sock != serv_sock and sock != s:
-            try:
-                sock.send(m)
-            except:
-                print "Unable to send message to client connected to: " + sock
-                print "Closing the connection..."
-                sock.close()
-                clients.remove(sock)
-                print "Connection closed."
+# server private/public key pair
+serv_pri_key_path = "server_keypair/server_private_key.pem"
+serv_pub_key_path = "server_keypair/server_public_key.pem"
+with open(serv_pri_key_path, "rb") as key_file:
+    serv_pri_key = key_file.read()
+with open(serv_pub_key_path, "rb") as key_file:
+    serv_pub_key = key_file.read()
 
-if __name__ == "__main__":
-    # default chatport is 9090
-    chatport = 9090
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], "h", ["sp="])
-    except getopt.GetoptError:
-        print "usage: chatserver.py -sp <portnumber>"
-        sys.exit()
-    for opt, arg in opts:
-        if opt == "-h":
-            # h is for help. it tells you how to use the program.
-            print "usage: chatserver.py -sp <portnumber>"
-            sys.exit()
-        if opt == "-sp":
-            chatport = arg
-    #list of connected clients (including the server)
-    clients = []
-    recv_buf = 4096
+# list of users and their associated passwords (IGNORING THE SALT FOR NOW)
+# username | salted-password | salt
+user_passes = {
+    'jack': common.Hash_This('4098'),
+    'sape': common.Hash_This('4139'),
+    'sap1': common.Hash_This('4132'),
+    'sap2': common.Hash_This('4136'),
+    'sap3': common.Hash_This('4134')
+}
 
-    # setting up the server socket
-    serv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    serv_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    serv_sock.bind(("0.0.0.0", chatport))
-    serv_sock.listen(10)
+# verifies a user
+def Verify_User(username, password):
+    if user_passes[username] == common.Hash_This(password):
+        return True
+    else:
+        return False
 
-    clients.append(serv_sock)
+# list of challenged users
+# IP | Port | Can send pass?
 
-    print "Server started on port: " + str(chatport)
-
-    while True:
-        # Gets all of the sockets that are ready
-        read_socks,write_socks,error_socks = select.select(clients,[],[])
-
-        for sock in read_socks:
-            if sock == serv_sock:
-                # new connections are received in the server socket
-                # accept the new connection and add it to the connection list
-                new_sock, address = serv_sock.accept()
-                clients.append(new_sock)
-                print "Client " + str(address) + " has connected."
-                # let everyone know that a new member has joined the chat
-                # broadcast(new_sock, "New member! Everyone say hi to: " + str(address))
-            else:
-                # data received from a client that is already connected.
-                try:
-                    data = sock.recv(recv_buf)
-                    if data:
-                        broadcast(sock, "\r" + "<FROM " + str(sock.getpeername()) + ">: " + data)
-                        print "<" + str(sock.getpeername()) + ">: " + data
-
-                except:
-                    # something went wrong, remove the client and close the socket.
-                    print "Client " + str(address) + " disconnected."
-                    sock.close()
-                    clients.remove(sock)
-                    continue
-
-    serv_sock.close()
+# list of currently connected users
+# username | IP | Port | Public Key | Shared AES key | Shared HKey | Nonce
