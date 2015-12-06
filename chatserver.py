fab3b409044d96087a8262ee2dccff8febaef73f
Joseph Ruane <jeruane@gmail.com>
2015-12-06 03:43:19 -0500

    Added user objects for various states of authentication
    also added methods for easily determining which state the
    authenticating user is in.

--- chatserver.py
+++ chatserver.py
@@ -1,7 +1,7 @@
 # chat server
 # python 3.5
 
-import sys, socket, getopt, select, common
+import sys, socket, getopt, select, common, os
 
 # server private/public key pair
 serv_pri_key_path = "server_keypair/server_private_key.pem"
@@ -21,6 +21,20 @@ user_passes = {
     'sap3': common.Hash_This('4134')
 }
 
+# list of challenged users
+# IP | Port | Challenge hash output(first 32bits) | Can send pass?
+users_challenged = [] # initially empty
+# example of how you add a user to this list when you receive a request for a challenge
+users_challenged.append(['129.0.0.1', '9090', os.urandom[:4], False])
+users_challenged.append(['129.0.0.2', '9090', os.urandom[:4], True])
+
+# list of currently connected users
+# username | IP | Port | Public Key | Shared AES key | Shared HKey | Nonce
+authed_users = [] # initially empty
+# example of how a user would be added to this list
+#                    username | IP       | Port | Public Key | Shared AES key | Shared HKey   | Nonce
+authed_users.append(['jack', '129.0.0.3', '9090', serv_pub_key, os.urandom(32), os.urandom(32), os.urandom(32)])
+
 # verifies a user
 def Verify_User(username, password):
     if user_passes[username] == common.Hash_This(password):
@@ -28,8 +42,63 @@ def Verify_User(username, password):
     else:
         return False
 
-# list of challenged users
-# IP | Port | Can send pass?
+# determines whether or not a user can answer a challenge yet.
+def Is_User_Challenged(ip, port):
+    for user in users_challenged:
+        print(user)
+        if user[0] == ip and user[1] == port:
+            return True
+    return False
 
-# list of currently connected users
-# username | IP | Port | Public Key | Shared AES key | Shared HKey | Nonce
+# returns a boolean saying whether or not a user can authenticate, ie has passed the challenge
+def Can_User_Auth(ip, port):
+    for user in users_challenged:
+        print(user)
+        if user[0] == ip and user[1] == port:
+            return user[2]
+    return False
+
+# not sure if this is good or not
+def Is_User_Authed(ip, port):
+    for user in authed_users:
+        print(user)
+        if user[0] == ip and user[1] == port:
+            return True
+    return False
+
+print(Is_User_Challenged('129.0.0.1', '9090'))
+print(Is_User_Challenged('129.0.0.1', '9091'))
+print(Is_User_Challenged('129.0.0.2', '9090'))
+
+print(Can_User_Auth('129.0.0.1', '9090'))
+print(Can_User_Auth('129.0.0.1', '9091'))
+print(Can_User_Auth('129.0.0.2', '9090'))
+
+
+
+
+#if __name__ == "__main__":
+#    # default chatport is 9090
+#    chatport = 9090
+#    try:
+#        opts, args = getopt.getopt(sys.argv[1:], "h", ["sp="])
+#    except getopt.GetoptError:
+#        print "usage: chatserver.py -sp <portnumber>"
+#        sys.exit()
+#    for opt, arg in opts:
+#        if opt == "-h":
+#            # h is for help. it tells you how to use the program.
+#            print "usage: chatserver.py -sp <portnumber>"
+#            sys.exit()
+#        if opt == "-sp":
+#            chatport = arg
+#    #list of connected clients (including the server)
+#    clients = []
+#    recv_buf = 4096
+#
+#    # setting up the server socket
+#    serv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#    serv_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+#    serv_sock.bind(("0.0.0.0", chatport))
+#    serv_sock.listen(10)
+#
